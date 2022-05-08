1bec1ddabd334f712b37d10d775f366365cb261f
Adelani <harounadelani@gmail.com>
2022-05-08 22:24:26 +0100

    new update

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+let btnS = document.querySelectorAll(".btns");
+
+for(let i=0; i<btnS.length; i++){
+    btnS[i].addEventListener("click", function(){
+        btnS[i].style.color = "hsl(0, 0%, 100%)"
+        btnS[i].style.backgroundColor = "hsl(25, 97%, 53%)"
+        
+    })
+    
+}
+
+
+
+
