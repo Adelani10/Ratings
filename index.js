4344f034dbda0fd8e2640859037788b0df023f68
Adelani <harounadelani@gmail.com>
2022-05-27 12:59:49 +0100

    change

--- index.js
+++ index.js
@@ -1,19 +1,50 @@
 
 
+const subBtn = document.querySelector(".sub-btn")
+const pickText = document.querySelector(".pick-text")
+const content = document.querySelector(".content")
 
+const qxt = document.querySelectorAll(".btns")
 
-
-let btnS = document.querySelectorAll(".btns");
-
-for(let i=0; i<btnS.length; i++){
-    btnS[i].addEventListener("click", function(){
-        btnS[i].style.color = "hsl(0, 0%, 100%)"
-        btnS[i].style.backgroundColor = "hsl(25, 97%, 53%)"
-        
-    })
-    
+const numArray = ["1", "2", "3", "4", "5"]
+for (let i = 0; i<numArray.length; i++){
+  
 }
 
 
+qxt.forEach(function(hello){
+    hello.addEventListener("click", function() {
+        qxt.forEach(function(item) {
+            if (item !== hello){
+                item.classList.remove("show-color")
+            }
+        })
+  
+    pickText.textContent = `You selected ${myText()} out of 5`
+
+  function myText(){
+  if (hello.classList.contains("btn-1")){
+    return numArray[0]
+  } 
+  else  if (hello.classList.contains("btn-2")){
+    return numArray[1]
+  }
+  else  if (hello.classList.contains("btn-3")){
+    return numArray[2]
+  } 
+  else  if (hello.classList.contains("btn-4")){
+    return numArray[3]
+  } 
+  else {
+    return numArray[4]
+  }
+  
+}
+        hello.classList.toggle("show-color")
+    })
+})
 
 
+subBtn.addEventListener("click", function(){
+  content.classList.add("show-typage")
+})
